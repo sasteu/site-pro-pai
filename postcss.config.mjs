66e0f14f4b1dd852523f513e94ada783d1ad7494
sateu <enricomatheus@outlook.com>
2025-07-08 08:55:09 -0300

    Corrige forma dos plugins no postcss.config.mjs para Next.js

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-const config = {
-  plugins: [tailwindcss, autoprefixer],
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
